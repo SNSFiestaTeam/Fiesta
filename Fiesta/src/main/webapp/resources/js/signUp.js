69b4dab062bbc0ed91393b10df28109d8d9f83d1
luejenie <luejenie@gmail.com>
2022-11-08 14:46:32 +0900

    20221108

--- Fiesta/src/main/webapp/resources/js/signUp.js
+++ Fiesta/src/main/webapp/resources/js/signUp.js
@@ -7,8 +7,8 @@
         -> 아이콘 변경 /제출 이벤트 제거, 유효하지 않은 입력으로 포커스 이동
 
 
-- 이메일(유효성 검사) : 아이콘 v/x
-- 성명(작성 여부) : v/x
+- 이메일(유효성 검사) : 아이콘 v(색변경)
+- 성명(작성 여부) : v(색변경)
 - 사용자이름(랜덤, 작성 여부) : <-, v/x
 - 비밀번호(유효성 검사 / 보이기숨기기) : v/x, eye
 - 비밀번호 확인(일치 여부) : v/x
@@ -45,22 +45,24 @@ const checkObj = {
 
 
 const memberEmail = document.getElementById("memberEmail");
-const memberEmailLabel = document.getElementById("memberEmailLabel");
+const memberName = document.getElementById("memberName");
+const memberNickname = document.getElementById("memberNickname");
+const memberPw = document.getElementById("memberPw");
+const memberPwConfirm = document.getElementById("memberPwConfirm");
 
-const emailCheck = document.getElementById("emailCheck");
-const emailXmark = document.getElementById("emailXmark");
 const signUpButton = document.getElementById("signUpButton");
 
 
 
+// 이메일 아이콘 : 필수입력, 유효성 검사
 memberEmail.addEventListener("input", function(){
+    const emailCheck = document.getElementById("emailCheck");
+    // const emailXmark = document.getElementById("emailXmark");
     
     // v/x 하지 말고, v 색 변경으로.
     if(memberEmail.value.trim().length == 0){ //문자가 입력되지 않을 때
         emailCheck.classList.add("iVisible", "gray");
         signUpButton.disabled = false;
-        // emailCheck.classList.add("iHidden");
-        // emailXmark.classList.add("iVisible", "red");
     } 
     
     // 유효성 검사
@@ -71,16 +73,28 @@ memberEmail.addEventListener("input", function(){
         emailCheck.classList.remove("gray");
         signUpButton.disabled = true;
         signUpButton.classList.add("buttonOn");
-        // emailCheck.classList.add("iVisible", "green");
-        // emailCheck.classList.remove("iHidden", "gray");
-        // emailXmark.classList.add("iHidden");
         
     } else {
         emailCheck.classList.add("iVisible", "gray");
         emailCheck.classList.remove("green");
-        signUpButton.disabled = false;
-        // emailCheck.classList.add("iHidden");
-        // emailXmark.classList.add("iVisible", "red");
     }
 });
 
+
+// 성명 아이콘 : 필수 입력
+memberName.addEventListener("input", function(){
+    const nameCheck = document.getElementById("nameCheck");
+
+    if(this.value.trim().length == 0){
+        nameCheck.classList.add("iVisible", "gray");
+        signUpButton.disabled = false;
+    
+    } else {
+        nameCheck.classList.add("iVisible", "green");
+        nameCheck.classList.remove("gray");
+
+
+    }
+
+
+})
