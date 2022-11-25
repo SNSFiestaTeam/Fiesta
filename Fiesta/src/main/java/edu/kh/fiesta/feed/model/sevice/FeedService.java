db39adc9cef45453d7023964cd3e453e072cfe2a
Shinamin_97 <qnrlgkals@naver.com>
2022-11-25 15:09:40 +0900

    팔로잉,팔로우 목록조회

--- Fiesta/src/main/java/edu/kh/fiesta/feed/model/sevice/FeedService.java
+++ Fiesta/src/main/java/edu/kh/fiesta/feed/model/sevice/FeedService.java
@@ -7,6 +7,16 @@ import edu.kh.fiesta.member.model.vo.Member;
 
 public interface FeedService {
 
+	/** 팔로일 목록 조회
+	 * @param memberNo
+	 * @return
+	 */
 	List<Member> selectFollowingList(int memberNo);
+
+	/** 팔로우 목록 조회
+	 * @param memberNo
+	 * @return
+	 */
+	List<Member> selectFollowList(int memberNo);
 
 }
