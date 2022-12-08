977ef2ba5daa015486e31b32fbd7f29d43eaee28
shin_bokyung <bboya0518@naver.com>
2022-12-08 14:27:46 +0900

    20221208 링크복사 기능

--- Fiesta/.history/src/main/webapp/resources/js/main/main_20221208141402.js
+++ Fiesta/.history/src/main/webapp/resources/js/main/main_20221208141402.js
@@ -131,8 +131,9 @@ const feedShareBtn = document.getElementById("feedShareBtn");
 
 feedShareBtn.addEventListener("click", function () {
   feedMenu.style.display = "none";
-  copy(boardNo);
-
+  share.style.display = "flex";
+  
+  body.classList.add("scrollLock");
 });
 
 
@@ -163,29 +164,7 @@ const feedUpdateBtnLogin = document.getElementById("feedUpdateBtnLogin");
 const feedShareBtnLogin = document.getElementById("feedShareBtnLogin");
 const feedSelectBtnLogin = document.getElementById("feedSelectBtnLogin");
 
-feedShareBtnLogin.addEventListener('click', () => {
-  
-  loginFeedMenu.style.display = "none";
-  copy(boardNo);
-
-})
-
-const copy = (text) => {
-  // 임시의 textarea 생성
-  const $textarea = document.createElement("textarea");
-
-  // body 요소에 존재해야 복사가 진행됨
-  document.body.appendChild($textarea);
-  
-  // 복사할 특정 텍스트를 임시의 textarea에 넣어주고 모두 셀렉션 상태
-  $textarea.value = "http://kh-classa.xyz/feedDetail/" + text;
-  // $textarea.value = "localhost/feedDetail/" + text;
-  $textarea.select();
-  
-  // 복사 후 textarea 지우기
-  document.execCommand('copy');
-  document.body.removeChild($textarea);
-}
+feedShareBtnLogin.addEventListener('click', (0 => {}))
 
 
 
--- Fiesta/.history/src/main/webapp/resources/js/main/main_20221208141845.js
+++ Fiesta/.history/src/main/webapp/resources/js/main/main_20221208141845.js
@@ -131,8 +131,9 @@ const feedShareBtn = document.getElementById("feedShareBtn");
 
 feedShareBtn.addEventListener("click", function () {
   feedMenu.style.display = "none";
-  copy(boardNo);
-
+  share.style.display = "flex";
+  
+  body.classList.add("scrollLock");
 });
 
 
@@ -178,8 +179,7 @@ const copy = (text) => {
   document.body.appendChild($textarea);
   
   // 복사할 특정 텍스트를 임시의 textarea에 넣어주고 모두 셀렉션 상태
-  $textarea.value = "http://kh-classa.xyz/feedDetail/" + text;
-  // $textarea.value = "localhost/feedDetail/" + text;
+  $textarea.value = "http://kh-classa.xyz/feed/" + text;
   $textarea.select();
   
   // 복사 후 textarea 지우기
